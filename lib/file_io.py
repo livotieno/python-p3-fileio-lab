16bdfbf53238c125b4734ac8c413ae2be918b963
Samuel Livio <livio5261@gmail.com>
2023-12-10 14:24:26 +0300

    complete

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,14 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", mode="w") as file:
+        write_data = file.write(file_content)
+        return write_data
+
 
 def append_file(file_name, append_content):
-    pass
-
+    with open(f"{file_name}.txt", mode="a") as file:
+        append_data = file.write(append_content)
+        return append_data
 def read_file(file_name):
-    pass
+    with open(f"{file_name}.txt", encoding="utf-8") as file:
+        read_data = file.read()
+        return read_data
